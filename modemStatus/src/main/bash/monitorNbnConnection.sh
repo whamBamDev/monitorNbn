ab038468ef310c90ede1a4d015367a53fddc6901
whamBamDev <spam.spam.spam@ineson.me>
2020-05-29 17:26:50 +1000

    Update to script that the link using a traceroute test to a static IP.

--- modemStatus/src/main/bash/monitorNbnConnection.sh
+++ modemStatus/src/main/bash/monitorNbnConnection.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 time_now=$(date +"%d-%m-%Y %T") 
 echo "==== start(001): ${time_now}"
-time_now=$(date +"%d-%m-%Y %T")
-echo "== traceroute - direct IP: ${time_now}"
-echo "tcptraceroute 203.36.190.11 80 -w 1 -m 10 -S"
-sudo tcptraceroute 203.36.190.11 80 -w 1 -m 10 -S
-echo "Exit Code:" $?
-echo ""
 
 time_now=$(date +"%d-%m-%Y %T")
-echo "== traceroute - DNS Lookup: ${time_now}"
-echo "tcptraceroute www.telstra.com.au 80 -w 1 -m 10 -S"
-sudo tcptraceroute www.telstra.com.au 80 -w 1 -m 10 -S
+echo "== traceroute - IP: ${time_now}"
+echo "tcptraceroute 203.50.5.178 80 -w 1"
+sudo tcptraceroute 203.50.5.178 80 -w 1
 echo "Exit Code: $?"
 echo ""
 
 time_now=$(date +"%d-%m-%Y %T")
-echo "== ping - direct IP: ${time_now}"
-echo "ping 203.36.190.11 -c 1"
-ping 203.36.190.11 -c 1
+echo "== traceroute - DNS Lookup: ${time_now}"
+echo "tcptraceroute www.telstra.com.au 80 -w 1"
+sudo tcptraceroute www.telstra.com.au 80 -w 1
 echo "Exit Code: $?"
 echo ""
 
