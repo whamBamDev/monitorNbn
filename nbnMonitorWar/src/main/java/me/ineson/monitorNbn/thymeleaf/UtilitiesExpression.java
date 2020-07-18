7cd6c1afb54df75892f5950e76c49e685c342ea4
whamBamDev <spam.spam.spam@ineson.me>
2020-07-19 00:08:56 +1000

    Add view utility for outputting lists.

--- nbnMonitorWar/src/main/java/me/ineson/monitorNbn/thymeleaf/UtilitiesExpression.java
+++ nbnMonitorWar/src/main/java/me/ineson/monitorNbn/thymeleaf/UtilitiesExpression.java
@@ -21,4 +21,25 @@ public class UtilitiesExpression {
 		return String.format("%02d:%02d:%02d", hours, minutes, seconds);
 	}
 
+	public String outputIterable(final Iterable<?>iterable) {
+		return outputIterable(iterable, "\n");
+	}
+
+	public String outputIterable(final Iterable<?>iterable, String lineEnd) {
+		
+		StringBuilder stringBuilder= new StringBuilder();
+		boolean firstObject = true;
+
+		for (Object object : iterable) {
+			if(!firstObject) {
+				stringBuilder.append(lineEnd);
+			}
+			firstObject = false;
+
+			stringBuilder.append(object);
+		}
+		
+		return stringBuilder.toString();
+	}
+	
 }
