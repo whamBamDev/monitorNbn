45d74445c7ffca48bdae0199ce98bfc068d86b3e
whamBamDev <spam.spam.spam@ineson.me>
2020-07-31 09:21:47 +1000

    When getting the modem status then increase the timeouts and add a retry

--- modemStatus/src/main/groovy/CheckModem.groovy
+++ modemStatus/src/main/groovy/CheckModem.groovy
@@ -5,15 +5,21 @@ import java.net.URL
 
 import me.ineson.monitorNbn.modemStatus.ModemStatus
 
-try {
-	String modemUrl = "http://192.168.0.1/";
-	println "Modem URL: " + modemUrl
+
+def retryAttempts = 3;
+
+while (retryAttempts > 0) {
+    try {
+		retryAttempts--
+        String modemUrl = "http://192.168.0.1/";
+        println "Modem URL: " + modemUrl
 	
-    InputStream modemPage = new URL( modemUrl).newInputStream( [connectTimeout: 5000, readTimeout: 1000]);
-
-    ModemStatus status = checkModem(modemPage, modemUrl);
-	println status
-} catch( Exception e) {
-	println "### Error: " + e.getMessage()
-}
-
+        InputStream modemPage = new URL( modemUrl).newInputStream( [connectTimeout: 10000, readTimeout: 5000]);
+		
+        ModemStatus status = checkModem(modemPage, modemUrl);
+        println status
+		retryAttempts = 0
+    } catch( Exception e) {
+	    println "### Error: " + e.getMessage()
+    }
+} 
